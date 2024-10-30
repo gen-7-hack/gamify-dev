d5b43de678aa5802c3b73906e1851f6ec5d8d5e7
Desiree <desiree2021325@gmail.com>
2024-10-30 17:28:23 +0800

    Build foundation for landing page
    Created the following components:
    - navigation bar that will direct the user to different pages
    - streaks to keep track how long the user has been using the app
    - challenge button that will lead to the challenge of the day
    - welcome card that welcomes the user to the app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,21 @@
 import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Landing from './pages/Landing.jsx'
+import Quests from './pages/Quests.jsx'
+import Leaderboard from './pages/Leaderboard.jsx'
+import Profile from './pages/Profile.jsx'
+import Challenge from './pages/Challenge.jsx'
 
 function App() {
   return (
     <Router>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
-      </nav>
       <Routes>
         <Route path="/" element={<Landing />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/quests" element={<Quests />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/challenge" element={<Challenge />} />
       </Routes>
     </Router>
   )
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -1,7 +1,72 @@
-import React from 'react';
+import React from 'react'
+import {Link} from "react-router-dom";
+import user from '../assets/images/user.png'
+import sunImage from '../assets/images/sun.png'
+import fireImage from '../assets/images/fire.png'
+import Navbar from '../components/Navbar.jsx'
+import '../css/Landing.css'
 
 const Landing = () => {
-  return <h1>Landing Page</h1>;
+  const userName = 'John' 
+  const days = 5
+
+  return (
+    <div className='phone-container'>
+      <div className='content'>
+        <div className='welcome'>
+          <Welcome name={userName} profilePic={user} />
+        </div>
+        <div className='streak'>
+          <Streak numDays={days} />
+        </div>
+        <div className='challenge'>
+          <ChallengeButton />
+        </div>
+      </div>
+      <div className='footer'>
+        <Navbar />
+      </div>
+    </div>
+  );
+}
+
+const Welcome = ({ name }) => {
+  return (
+      <div className="welcome-card">
+          <div className="welcome-text">
+              <div className="welcome-back">
+                  <img src={sunImage} className="sun-img" />
+                  <h3>WELCOME BACK!</h3>
+              </div>
+              <h2>Nice seeing you here, {name}</h2>
+          </div>
+      </div>
+  );
+}
+
+const Streak = ({ numDays }) => {
+  return (
+    <div className='streak-card'>
+      <button className='streak-button'>
+        <div className='streak-text'>
+          <h3>{numDays} days in a row!</h3>
+        </div>
+        <img src={fireImage} className='fire-img' />
+      </button>
+    </div>
+  );
+}
+
+const ChallengeButton = () => {
+  return (
+    <div>
+      <Link to={"/challenge"}>
+        <button className='challenge-button'>
+          <h3>Challenge!</h3>
+        </button>
+      </Link>
+    </div>
+  )
 }
 
 export default Landing;
